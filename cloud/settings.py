ecf0b491a3a009f16beecd825001308adc0f39d1
nitinprakash96 <prakash.nitin63@gmail.com>
2016-10-17 17:47:58 +0530

    activated sample model

--- cloud/settings.py
+++ cloud/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'cloudplay.apps.CloudplayConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
