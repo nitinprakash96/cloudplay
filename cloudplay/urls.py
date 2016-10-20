6477b9b3ac9f6df5c1731c3be7c0f0549c9be839
nitinprakash96 <prakash.nitin63@gmail.com>
2016-10-20 18:37:00 +0530

    sample template added

--- cloudplay/urls.py
+++ cloudplay/urls.py
@@ -7,5 +7,9 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index, name=''),
+	# /music/
+    url(r'^$', views.index, name='index'),
+
+    # /music/712/
+    url(r'^(?P<album_id>[0-9]+)/$', views.detail, name = 'detail'),
 ]
