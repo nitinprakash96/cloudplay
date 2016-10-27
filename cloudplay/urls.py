b97a37c935f432b8aa43239dcddc0c65e0647b86
nitinprakash96 <prakash.nitin63@gmail.com>
2016-10-28 00:55:29 +0530

    added url pattern to navigate teh favourites list

--- cloudplay/urls.py
+++ cloudplay/urls.py
@@ -15,6 +15,6 @@ urlpatterns = [
     # /music/<album_id>/
     url(r'^(?P<album_id>[0-9]+)/$', views.detail, name = 'detail'),
 
-    # /music/<album_id>/favourite
-    url(r'^(?P<album_id>[0-9]+)/$', views.favourite, name = 'favourite'),
+    # /music/<album_id>/favourite/
+    url(r'^(?P<album_id>[0-9]+)/favourite/$', views.favourite, name = 'favourite'),
 ]
